91b207c07368758891b80c6d554931319c3b643e
NestorSanchez <nesaenz.69@gmail.com>
2022-08-16 21:43:06 -0500

    add new component NavBar

---
+++ components/NavBar.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+export const NavBar = () => {
+  return (
+    <nav>
+      <Link>Inicio</Link>
+      <Link>Nuestra Empresa</Link>
+      <Link>Servicios</Link>
+      <Link>Servicios</Link>
+      <Link>Regulacion</Link>
+      <Link>Preguntas Frecuentes</Link>
+      <Link>Internet Sano</Link>
+      <Link>Contactenos</Link>
+    </nav>
+  );
+};
