76d38920cc853728f312e62d9ed016ed8d89268a
NestorSanchez <nesaenz.69@gmail.com>
2022-08-17 21:14:05 -0500

    add bootstrap

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,16 +1,35 @@
 import Link from "next/link";
 import React from "react";
 
+const style = {
+  color: "#0070f3",
+  textDecoration: "underline",
+};
+
 export const NavBar = () => {
   return (
     <nav>
-      <Link href="/">Inicio</Link>
-      <Link href="nuestraempresa">Nuestra Empresa</Link>
-      <Link href="servicios">Servicios</Link>
-      <Link href="regulacion">Regulacion</Link>
-      <Link href="preguntas">Preguntas Frecuentes</Link>
-      <Link href="internetsano">Internet Sano</Link>
-      <Link href="contactenos">Contactenos</Link>
+      <Link href="/">
+        <a style={style}>Inicio</a>
+      </Link>
+      <Link href="nuestraempresa">
+        <a>Nuestra Empresa</a>
+      </Link>
+      <Link href="servicios">
+        <a>Servicios</a>
+      </Link>
+      <Link href="regulacion">
+        <a>Regulacion</a>
+      </Link>
+      <Link href="preguntas">
+        <a>Preguntas Frecuentes</a>
+      </Link>
+      <Link href="internetsano">
+        <a>Internet Sano</a>
+      </Link>
+      <Link href="contactenos">
+        <a>Contactenos</a>
+      </Link>
     </nav>
   );
 };
